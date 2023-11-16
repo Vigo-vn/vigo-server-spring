0e41165722c1ca255c36b5daf711f1b30dc317f2
Le Viet Dat <lvdat.roy@gmail.com>
2023-11-16 20:55:43 +0700

    refactor: use lombok

--- src/main/java/uet/vnu/vigo/models/User.java
+++ src/main/java/uet/vnu/vigo/models/User.java
@@ -6,8 +6,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Table(name = "users")
 public class User {
     @Id
@@ -18,36 +27,4 @@ public class User {
     private String username;
     @Column
     private String password;
-
-    public User() {
-    }
-
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
